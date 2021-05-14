a8d6fdf28e2dfae92ef979434fa5ebfdf5a42011
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-05-13 22:19:00 -0400

    done

--- src/__tests__/Links.test.js
+++ src/__tests__/Links.test.js
@@ -1,21 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
+import React from "react";
 
-import Links from "../components/Links";
+function Links(props) {
+  return (
+    <div id="links">
+      <h3> Links </h3>
+      <a href = {props.github}> {props.github}  </a> 
+      <a href = {props.linkedin}> {props.linkedin} </a>
+    </div>
+  );
+}
 
-test("renders the h3 with the text 'Links'", () => {
-  render(<Links />);
-  expect(screen.queryByText("Links")).toBeInTheDocument();
-});
-
-test("displays a Github link passed down as a prop", () => {
-  render(<Links github={"https://github.com/liza"} />);
-  expect(screen.queryByText("https://github.com/liza")).toBeInTheDocument();
-});
-
-test("displays a Linkedin link passed down as a prop", () => {
-  render(<Links linkedin={"https://www.linkedin.com/in/liza/"} />);
-  expect(
-    screen.queryByText("https://www.linkedin.com/in/liza/")
-  ).toBeInTheDocument();
-});
+export default Links;
